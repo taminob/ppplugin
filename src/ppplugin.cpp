5f69f79c49a5139534edb037cdbb60327643f84e
Tamino Bauknecht <dev@tb6.eu>
2023-12-19 22:24:55 +0100

    ppplugin: Add definitions for segfault_handling

--- src/ppplugin.cpp
+++ src/ppplugin.cpp
@@ -1,2 +1,8 @@
 #include "plugin.h"
 #include "plugin_manager.h"
+#include "detail/segfault_handling.h"
+
+namespace ppplugin::detail::segfault_handling {
+std::mutex segfault_checker_mutex;
+std::jmp_buf segfault_recover_point;
+} // namespace ppplugin::detail::segfault_handling
