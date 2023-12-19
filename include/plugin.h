67e516b865926c01131b8c05d00ebbd002ed6fa0
Tamino Bauknecht <dev@tb6.eu>
2023-12-19 19:40:47 +0100

    plugin.h: Catch segfaults for CPlugin and other improvements

--- include/plugin.h
+++ include/plugin.h
@@ -1,6 +1,7 @@
 #ifndef PPPLUGIN_PLUGIN_H
 #define PPPLUGIN_PLUGIN_H
 
+#include "detail/segfault_handling.h"
 #include "detail/template_helpers.h"
 #include "lua_script.h"
 
@@ -16,37 +17,28 @@
 
 namespace ppplugin {
 #ifndef PPPLUGIN_CPP17_COMPATIBILITY
-// TODO: does not work
-template <typename P, typename... ReturnValues, typename... Args>
-concept CallableWithAnything(P p)
-{
-    {
-        p.call<ReturnValues...>(std::declval<Args...>())
-    } -> std::tuple<ReturnValues...>
-}
 template <typename P>
 concept IsPlugin = requires(P p) {
     {
         std::convertible_to<P, bool>
-    }
-    ,
-        { CallableWithAnything<P, int> },
-        { P::create() },
-}
+    };
+    // {
+    //     p.template call<>(std::declval<int>())
+    // } -> std::same_as<std::tuple<>>;
+};
 #endif // PPPLUGIN_CPP17_COMPATIBILITY
 
 template <typename... Plugins>
-class GenericPlugin
 #ifndef PPPLUGIN_CPP17_COMPATIBILITY
     requires(IsPlugin<Plugins> && ...)
 #endif // PPPLUGIN_CPP17_COMPATIBILITY
-{
+class GenericPlugin {
 
 public:
     GenericPlugin() = default;
     template <typename P,
         typename = std::enable_if_t<(std::is_base_of_v<Plugins, P> || ...)>>
-    GenericPlugin(P && plugin) // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
+    GenericPlugin(P&& plugin) // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
         : plugin_(std::forward<P>(plugin))
     {
     }
@@ -65,11 +57,13 @@ public:
 
     // TODO: add specialization for void return type (empty tuple triggers nodiscard)
     template <typename... ReturnValues, typename... Args>
-    [[nodiscard]] std::tuple<ReturnValues...> call(Args && ... args)
+    [[nodiscard]] std::tuple<ReturnValues...> call(const std::string& function_name, Args&&... args)
     {
-        return std::visit([&args...](auto& plugin) -> std::tuple<ReturnValues...> {
-            return plugin.template call<ReturnValues...>(std::forward<Args>(args)...);
-        },
+        return std::visit(
+            [&function_name, &args...](auto& plugin) -> std::tuple<ReturnValues...> {
+                return plugin.template call<ReturnValues...>(
+                    function_name, std::forward<Args>(args)...);
+            },
             plugin_);
     }
 
@@ -135,10 +129,10 @@ public:
             throw std::runtime_error("symbol not valid"); // TODO
         }
         if constexpr (std::is_same_v<ReturnValue, void>) {
-            function(args...);
+            function(std::forward<Args>(args)...);
             return std::tuple<> {};
         } else {
-            return std::tuple<ReturnValue> { function(args...) };
+            return std::tuple<ReturnValue> { function(std::forward<Args>(args)...) };
         }
     }
 
@@ -163,6 +157,9 @@ protected:
     }
 
 protected:
+    auto& plugin() { return plugin_; }
+
+private:
     boost::dll::shared_library plugin_;
     LoadingError loading_error_ { LoadingError::none };
     RuntimeError runtime_error_ { RuntimeError::none };
@@ -175,47 +172,97 @@ public:
     {
     }
 
+    template <typename T>
+    struct CopyIfReference {
+        template <typename = std::enable_if<std::is_reference_v<T>>>
+        explicit CopyIfReference(std::remove_reference<T> by_value)
+            : value { by_value }
+        {
+        }
+        template <typename = std::enable_if<!std::is_reference_v<T>>>
+        explicit CopyIfReference(T&& by_reference)
+            : value { by_reference }
+        {
+        }
+
+        T value;
+    };
+
     template <typename... ReturnValues, typename... Args>
     [[nodiscard]] auto call(const std::string& function_name, Args&&... args)
+    {
+        return internalCall<false, ReturnValues...>(
+            function_name, std::forward<Args>(args)...);
+    }
+
+    template <typename... ReturnValues, typename... Args>
+    [[nodiscard]] auto safeCall(const std::string& function_name, Args&&... args)
+    {
+        return internalCall<true, ReturnValues...>(
+            function_name, std::forward<Args>(args)...);
+    }
+
+protected:
+    template <bool catchSegfaults, typename... ReturnValues, typename... Args>
+    [[nodiscard]] auto internalCall(const std::string& function_name, Args&&... args)
     {
         // TODO: invalid number of arguments can cause segfault
         // TODO: think of better solution than this
         static_assert(sizeof...(ReturnValues) <= 1,
             "C does not support more than one return value! "
             "Consider wrapping the types into an std::tuple.");
+        static_assert(!(std::is_reference_v<ReturnValues> || ...),
+            "C does not support references for its return value!");
+        static_assert(!(std::is_reference_v<Args> || ...),
+            "C does not support references for its arguments! "
+            "Consider passing the argument with an explicit cast to the desired type.");
         using ReturnValue = detail::templates::FirstOrT<void, ReturnValues...>;
         using FunctionType = ReturnValue (*)(Args...);
         // TODO: check ABI compatibility (same compiler + major version)?
-        if (!plugin_.has(function_name)) {
+        if (!plugin().has(function_name)) {
             throw std::runtime_error("symbol not found"); // TODO
         }
         // cannot use boost here because boost::dll expects the symbol to be
         // a variable (pointing to the function) not a function
 #if BOOST_OS_WINDOWS
         auto function = reinterpret_cast<FunctionType>(
-            boost::winapi::get_proc_address(plugin_.native(), function_name.c_str()));
+            boost::winapi::get_proc_address(plugin().native(), function_name.c_str()));
 #else
         // no other way to convert void* to function pointer
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
         auto function = reinterpret_cast<FunctionType>(
-            dlsym(plugin_.native(), function_name.c_str()));
+            dlsym(plugin().native(), function_name.c_str()));
 #endif // BOOST_OS_WINDOWS
         if (!function) {
-            loading_error_ = LoadingError::symbolInvalid;
             throw std::runtime_error("symbol not valid"); // TODO
         }
         if constexpr (std::is_same_v<ReturnValue, void>) {
-            function(args...);
+            if constexpr (catchSegfaults) {
+                detail::segfault_handling::exec<ReturnValue>(function, std::forward<Args>(args)...);
+            } else {
+                function(std::forward<Args>(args)...);
+            }
             return std::tuple<> {};
         } else {
-            return std::tuple<ReturnValue> { function(args...) };
+            if constexpr (catchSegfaults) {
+                return std::tuple<ReturnValue> {
+                    detail::segfault_handling::exec<ReturnValue>(function, std::forward<Args>(args)...)
+                };
+            } else {
+                return std::tuple<ReturnValue> { function(std::forward<Args>(args)...) };
+            }
         }
     }
 };
 
+/**
+ * Plugin that has a creator function that creates an object
+ * of a known base class.
+ */
 template <typename P>
 class CppObjectPlugin {
 public:
+    // TODO
 };
 
 class LuaPlugin {
