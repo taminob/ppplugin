b7551cfa65dcd88fd9c9eb690c7e0399a074ea01
Tamino Bauknecht <dev@tb6.eu>
2024-01-01 18:33:28 +0100

    c: Use boost::dll instead of platform-dependent fix

--- include/c/plugin.h
+++ include/c/plugin.h
@@ -20,25 +20,12 @@ public:
         static_assert(!(std::is_reference_v<Args> || ...),
             "C does not support references for its arguments! "
             "Consider passing the argument with an explicit cast to the desired type.");
-        using FunctionType = ReturnValue (*)(Args...);
+        using FunctionType = ReturnValue(Args...);
 
         if (!plugin().has(function_name)) {
             throw std::runtime_error("symbol not found"); // TODO
         }
-        // cannot use boost here because boost::dll expects the symbol to be
-        // a variable (pointing to the function) not a function
-#if BOOST_OS_WINDOWS
-        auto function = reinterpret_cast<FunctionType>(
-            boost::winapi::get_proc_address(plugin().native(), function_name.c_str()));
-#else
-        // no other way to convert void* to function pointer
-        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-        auto function = reinterpret_cast<FunctionType>(
-            dlsym(plugin().native(), function_name.c_str()));
-#endif // BOOST_OS_WINDOWS
-        if (!function) {
-            throw std::runtime_error("symbol not valid"); // TODO
-        }
+        auto function = boost::dll::import_symbol<FunctionType>(plugin(), function_name);
         return function(std::forward<Args>(args)...);
     }
 };
