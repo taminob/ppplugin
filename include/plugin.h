2c143eadf4b2ea43e9ff071a2436f5ede048292b
Tamino Bauknecht <dev@tb6.eu>
2024-01-17 22:45:44 +0100

    GenericPlugin: Move member function definitions out of class definition
    Additionally, fix C++20 concept for Plugin interface.

--- include/plugin.h
+++ include/plugin.h
@@ -1,6 +1,11 @@
 #ifndef PPPLUGIN_PLUGIN_H
 #define PPPLUGIN_PLUGIN_H
 
+#include "c/plugin.h"
+#include "cpp/plugin.h"
+#include "lua/plugin.h"
+#include "noop_plugin.h"
+
 #include "detail/template_helpers.h"
 
 #include <boost/dll.hpp>
@@ -16,30 +21,27 @@
 namespace ppplugin {
 #ifndef PPPLUGIN_CPP17_COMPATIBILITY
 template <typename P>
-concept IsPlugin = requires(P p) {
+concept IsPlugin = requires(P plugin) {
     {
-        std::convertible_to<P, bool>
+        std::convertible_to<const P, bool>
     };
-    // {
-    //     p.template call<>(std::declval<int>())
-    // } -> std::same_as<std::tuple<>>;
+    {
+        plugin.template call<void>(std::declval<std::string>())
+    } -> std::same_as<void>;
 };
 #endif // PPPLUGIN_CPP17_COMPATIBILITY
 
 template <typename... Plugins>
-#ifndef PPPLUGIN_CPP17_COMPATIBILITY
-    requires(IsPlugin<Plugins> && ...)
-#endif // PPPLUGIN_CPP17_COMPATIBILITY
 class GenericPlugin {
-
 public:
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+    static_assert((IsPlugin<Plugins> && ...), "All plugins must adhere to the Plugin interface.");
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
+
     GenericPlugin() = default;
     template <typename P,
         typename = std::enable_if_t<(std::is_base_of_v<Plugins, P> || ...)>>
-    GenericPlugin(P&& plugin) // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
-        : plugin_(std::forward<P>(plugin))
-    {
-    }
+    GenericPlugin(P&& plugin); // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
 
     virtual ~GenericPlugin() = default;
     GenericPlugin(const GenericPlugin&) = delete;
@@ -48,40 +50,57 @@ public:
     GenericPlugin& operator=(GenericPlugin&&) noexcept = default;
 
     // NOLINTNEXTLINE(google-explicit-constructor,hicpp-explicit-conversions)
-    virtual operator bool() const
-    {
-        return std::visit([](const auto& plugin) -> bool { return plugin; }, plugin_);
-    }
+    operator bool() const;
 
-    // TODO: add specialization for void return type (empty tuple triggers nodiscard)
+    template <typename... Args>
+    void call(const std::string& function_name, Args&&... args);
     template <typename ReturnValue, typename... Args>
-    [[nodiscard]] ReturnValue call(const std::string& function_name, Args&&... args)
-    {
-        return std::visit(
-            [&function_name, &args...](auto& plugin) {
-                return plugin.template call<ReturnValue>(
-                    function_name, std::forward<Args>(args)...);
-            },
-            plugin_);
-    }
+    [[nodiscard]] ReturnValue call(const std::string& function_name, Args&&... args);
 
     template <typename P>
-    std::optional<P> plugin()
-    {
-        if (auto* plugin = std::get_if<P>(plugin_)) {
-            return *plugin;
-        }
-        return std::nullopt;
-    }
+    std::optional<std::reference_wrapper<P>> plugin();
 
 private:
     detail::templates::UniqueVariant<Plugins...> plugin_;
 };
 
-class LuaPlugin;
-class CppPlugin;
-class CPlugin;
-using Plugin = GenericPlugin<CPlugin, CppPlugin, LuaPlugin>;
+using Plugin = GenericPlugin<CPlugin, CppPlugin, LuaPlugin, NoopPlugin>;
+
+template <typename... Plugins>
+template <typename P, typename>
+GenericPlugin<Plugins...>::GenericPlugin(P&& plugin)
+    : plugin_(std::forward<P>(plugin))
+{
+}
+
+template <typename... Plugins>
+GenericPlugin<Plugins...>::operator bool() const
+{
+    return std::visit([](const auto& plugin) -> bool { return plugin; }, plugin_);
+}
+
+template <typename... Plugins>
+template <typename ReturnValue, typename... Args>
+ReturnValue GenericPlugin<Plugins...>::call(const std::string& function_name, Args&&... args)
+{
+    return std::visit(
+        [&function_name, &args...](auto& plugin) {
+            return plugin.template call<ReturnValue>(
+                function_name, std::forward<Args>(args)...);
+        },
+        plugin_);
+}
+
+template <typename... Plugins>
+template <typename P>
+std::optional<std::reference_wrapper<P>> GenericPlugin<Plugins...>::plugin()
+{
+    if (auto* plugin = std::get_if<P>(plugin_)) {
+        return *plugin;
+    }
+    return std::nullopt;
+}
+
 } // namespace ppplugin
 
 #endif // PPPLUGIN_PLUGIN_H
