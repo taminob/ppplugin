829165abf37ea0e15f90228fc7c5e2ba33f1a106
Tamino Bauknecht <dev@tb6.eu>
2024-01-21 01:20:36 +0100

    test: Add forEachCombination test helper

---
+++ test/test_helper.h
@@ -0,0 +1,64 @@
+#ifndef PPPLUGIN_TEST_HELPER_H
+#define PPPLUGIN_TEST_HELPER_H
+
+#include "detail/template_helpers.h"
+
+#include <array>
+#include <tuple>
+#include <variant>
+
+namespace ppplugin::test {
+namespace detail {
+    template <std::size_t outerIndex, std::size_t innerIndex,
+        typename Func, typename TupleOne, typename TupleTwo>
+    constexpr void forEachCombinationImpl(Func&& func,
+        TupleOne&& outer_args, TupleTwo&& inner_args)
+    {
+        constexpr auto OUTER_SIZE = std::tuple_size<TupleOne>();
+        constexpr auto INNER_SIZE = std::tuple_size<TupleTwo>();
+        if constexpr (innerIndex >= INNER_SIZE) {
+            if constexpr (outerIndex < OUTER_SIZE) {
+                forEachCombinationImpl<outerIndex + 1, 0>(std::forward<Func>(func),
+                    std::forward<TupleOne>(outer_args),
+                    std::forward<TupleTwo>(inner_args));
+            }
+        } else if constexpr (outerIndex < OUTER_SIZE) {
+
+            auto&& a = std::get<outerIndex>(outer_args);
+            auto&& b = std::get<innerIndex>(inner_args);
+            using A = decltype(a);
+            using B = decltype(b);
+            if constexpr (std::is_invocable_v<Func, A, B>) {
+                func(a, b);
+            } else if constexpr (std::is_invocable_v<Func, A, B, std::size_t>) {
+                func(a, b, outerIndex * OUTER_SIZE + innerIndex);
+            } else if constexpr (std::is_invocable_v<Func,
+                                     A, B, std::size_t, std::size_t>) {
+                func(a, b, outerIndex, innerIndex);
+            } else {
+                static_assert(!sizeof(Func), "No known way to call given function!");
+            }
+
+            if constexpr (innerIndex < INNER_SIZE) {
+                forEachCombinationImpl<outerIndex, innerIndex + 1>(std::forward<Func>(func),
+                    std::forward<TupleOne>(outer_args),
+                    std::forward<TupleTwo>(inner_args));
+            }
+        }
+    }
+} // namespace detail
+
+constexpr void noop() { }
+
+template <typename Func, typename... Args>
+constexpr void forEachCombination(Func&& func, Args&&... args)
+{
+    if constexpr (sizeof...(Args) > 0) {
+        detail::forEachCombinationImpl<0, 0>(std::forward<Func>(func),
+            std::make_tuple(std::forward<Args>(args)...),
+            std::make_tuple(std::forward<Args>(args)...));
+    }
+}
+} // namespace ppplugin::test
+
+#endif // PPPLUGIN_TEST_HELPER_H
