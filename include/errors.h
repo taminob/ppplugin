1c49eb5ce14a765ed36c5b8c6d64db9c60395e9a
Tamino Bauknecht <dev@tb6.eu>
2024-03-05 21:07:20 +0100

    errors: Add std::source_location to CallError

--- include/errors.h
+++ include/errors.h
@@ -1,9 +1,13 @@
 #ifndef PPPLUGIN_ERRORS_H
 #define PPPLUGIN_ERRORS_H
 
+#include "detail/compatibility_utils.h"
 #include "expected.h"
 
 #include <memory>
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+#include <source_location>
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
 #include <string>
 
 namespace ppplugin {
@@ -16,19 +20,48 @@ public:
     };
 
     // NOLINTNEXTLINE(google-explicit-constructor,hicpp-explicit-conversions)
-    CallError(Code error_code)
+    CallError(Code error_code
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+        ,
+        const std::source_location& location = std::source_location::current()
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
+            )
         : error_ { error_code }
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+        , location_ { location }
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
     {
     }
 
-    CallError(Code error, std::string_view what)
+    CallError(Code error, std::string_view what
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+        ,
+        const std::source_location& location = std::source_location::current()
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
+            )
         : error_ { error }
         , what_ { what }
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+        , location_ { location }
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
     {
     }
 
     [[nodiscard]] CallError error() const { return error_; }
     [[nodiscard]] const std::string& what() const { return what_; }
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+    [[nodiscard]] const std::source_location& location() const
+    {
+        return location_;
+    }
+    [[nodiscard]] std::string where() const
+    {
+        return format("{}:{} in {}",
+            location_.file_name(),
+            location_.line(),
+            location_.function_name());
+    }
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
 
     friend constexpr bool operator==(const CallError& lhs, const CallError& rhs)
     {
@@ -38,6 +71,9 @@ public:
 private:
     Code error_;
     std::string what_;
+#ifndef PPPLUGIN_CPP17_COMPATIBILITY
+    std::source_location location_;
+#endif // PPPLUGIN_CPP17_COMPATIBILITY
 };
 
 enum class LoadError {
