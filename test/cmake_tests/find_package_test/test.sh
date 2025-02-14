3b55fa2dba72eefdb9a0a1ca6f202a44c2356604
Tamino Bauknecht <dev@tb6.eu>
2025-02-14 21:56:11 +0100

    test: Add additional cmake_tests for expected failure
    These tests are expected to fail because they try to use the shared
    library when only the static version is installed and the other way
    around.

--- test/cmake_tests/find_package_test/test.sh
+++ test/cmake_tests/find_package_test/test.sh
@@ -7,22 +7,22 @@ ROOT_SOURCE_DIR="${1}"
 BUILD_DIR="${2}"
 INSTALL_ROOT="${3}"
 
-if [ "${4}" = "SHARED" ]; then
-    TEST_SHARED=ON
-elif [ "${4}" = "STATIC" ]; then
-    TEST_SHARED=OFF
+if [ "${4}" = "SHARED" ] || [ "${4}" = "STATIC_FAIL" ]; then
+  TEST_SHARED=ON
+elif [ "${4}" = "STATIC" ] || [ "${4}" = "SHARED_FAIL" ]; then
+  TEST_SHARED=OFF
 else
-    echo "Fourth argument must be 'SHARED' or 'STATIC'"
-    return
+  echo "Fourth argument must be 'SHARED', 'STATIC', 'SHARED_FAIL' or 'STATIC_FAIL'"
+  return
 fi
 
 if [ "${5}" = "Cpp17" ]; then
-    TEST_CPP17_COMPATIBILITY=ON
+  TEST_CPP17_COMPATIBILITY=ON
 elif [ "${5}" = "Cpp20" ]; then
-    TEST_CPP17_COMPATIBILITY=OFF
+  TEST_CPP17_COMPATIBILITY=OFF
 else
-    echo "Fifth argument must be 'Cpp17' or 'Cpp20'"
-    return
+  echo "Fifth argument must be 'Cpp17' or 'Cpp20'"
+  return
 fi
 
 TEST_BUILD_DIR="${BUILD_DIR}/shared_${TEST_SHARED}_cpp17_${TEST_CPP17_COMPATIBILITY}"
@@ -30,8 +30,17 @@ EXECUTABLE_NAME="${TEST_BUILD_DIR}/a"
 
 "${ROOT_SOURCE_DIR}/test/cmake_tests/build_and_install_ppplugin.sh" "${ROOT_SOURCE_DIR}" "${BUILD_DIR}/ppplugin_build_shared_${TEST_SHARED}" "${INSTALL_ROOT}" "${TEST_SHARED}"
 
+# if it is a test with expected failure, switch dependency to opposite library type to provoke error
+if [ "${4}" = "SHARED_FAIL" ]; then
+  TEST_SHARED=ON
+  TEST_BUILD_DIR="${TEST_BUILD_DIR}_shared_fail"
+elif [ "${4}" = "STATIC_FAIL" ]; then
+  TEST_SHARED=OFF
+  TEST_BUILD_DIR="${TEST_BUILD_DIR}_static_fail"
+fi
+
 CMAKE_PREFIX_PATH="${INSTALL_ROOT}/usr/local/lib/cmake" \
-    cmake . --log-level DEBUG -B "${TEST_BUILD_DIR}" -DPPPLUGIN_SHARED="${TEST_SHARED}" -DPPPLUGIN_ENABLE_CPP17_COMPATIBILITY="${TEST_CPP17_COMPATIBILITY}"
+  cmake . --log-level DEBUG -B "${TEST_BUILD_DIR}" -DPPPLUGIN_SHARED="${TEST_SHARED}" -DPPPLUGIN_ENABLE_CPP17_COMPATIBILITY="${TEST_CPP17_COMPATIBILITY}"
 cmake --build "${TEST_BUILD_DIR}"
 
 "${EXECUTABLE_NAME}"
