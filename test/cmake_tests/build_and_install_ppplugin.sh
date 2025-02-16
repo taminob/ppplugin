3533f7f6f537901c251e9699b5f0d7715e71b82b
Tamino Bauknecht <dev@tb6.eu>
2025-02-17 00:31:46 +0100

    test: Compile library with cpp17 compatibility in cmake_tests if necessary

--- test/cmake_tests/build_and_install_ppplugin.sh
+++ test/cmake_tests/build_and_install_ppplugin.sh
@@ -7,11 +7,12 @@ ROOT_SOURCE_DIR="${1}"
 BUILD_DIR="${2}"
 INSTALL_ROOT="${3}"
 BUILD_SHARED="${4}"
+BUILD_CPP17_COMPATIBLE="${5}"
 
 echo "Building ppplugin library (shared: ${BUILD_SHARED})..."
 
 echo "Use sources at '${ROOT_SOURCE_DIR}'..."
-cmake "${ROOT_SOURCE_DIR}" -B "${BUILD_DIR}" -DPPPLUGIN_SHARED="${BUILD_SHARED}"
+cmake "${ROOT_SOURCE_DIR}" -B "${BUILD_DIR}" -DPPPLUGIN_SHARED="${BUILD_SHARED}" -DPPPLUGIN_ENABLE_CPP17_COMPATIBILITY="${BUILD_CPP17_COMPATIBLE}"
 echo "Build in '${BUILD_DIR}'..."
 cmake --build "${BUILD_DIR}"
 echo "Install to '${INSTALL_ROOT}'..."
