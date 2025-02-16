78f394188d86199660c992487625f6f84acb800d
Tamino Bauknecht <dev@tb6.eu>
2025-02-17 00:32:27 +0100

    lua|Expected: Minor include fixes

--- include/ppplugin/errors.h
+++ include/ppplugin/errors.h
@@ -4,7 +4,6 @@
 #include "ppplugin/detail/compatibility_utils.h"
 #include "ppplugin/expected.h"
 
-#include <memory>
 #ifndef PPPLUGIN_CPP17_COMPATIBILITY
 #include <source_location>
 #endif // PPPLUGIN_CPP17_COMPATIBILITY
