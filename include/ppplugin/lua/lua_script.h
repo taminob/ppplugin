fe94da9962d8e0b84b8af6f3c0c54b2bdd5b54d5
Tamino Bauknecht <dev@tb6.eu>
2025-03-30 22:32:45 +0200

    everything: Move remaining member function definitions out of class definition
    Most member functions were already at the bottom of the header files or
    in their own source file. This commit moves the remaining definitions
    there as well.

--- include/ppplugin/lua/lua_script.h
+++ include/ppplugin/lua/lua_script.h
@@ -28,63 +28,26 @@ public:
     LuaScript& operator=(LuaScript&&) noexcept = default;
 
     template <typename ReturnValue, typename... Args>
-    CallResult<ReturnValue> call(const std::string& function_name, Args&&... args)
-    {
-        if (state_.pushGlobal(function_name)) {
-            if (auto function = state_.top<ReturnValue(Args...)>()) {
-                return (*function)(std::forward<Args>(args)...);
-            }
-            return CallError { CallError::Code::unknown, "Symbol does not match given type" };
-        }
-        return { CallError::Code::symbolNotFound };
-    }
+    [[nodiscard]] CallResult<ReturnValue> call(const std::string& function_name, Args&&... args);
 
     template <typename VariableType>
-    CallResult<VariableType> global(const std::string& variable_name)
-    {
-        if (state_.pushGlobal(variable_name)) {
-            if (auto value = state_.pop<VariableType>()) {
-                return value.value();
-            }
-            return { CallError::Code::incorrectType };
-        }
-        return { CallError::Code::symbolNotFound };
-    }
+    [[nodiscard]] CallResult<VariableType> global(const std::string& variable_name);
     template <typename VariableType>
-    void global(const std::string& variable_name, VariableType&& new_value)
-    {
-        state_.push(new_value);
-        state_.markGlobal(variable_name);
-    }
+    void global(const std::string& variable_name, VariableType&& new_value);
 
 private:
     LuaScript();
 
-    bool run();
+    [[nodiscard]] bool run();
 
-    std::optional<LoadError> loadFile(const std::filesystem::path& lua_file, bool auto_run);
+    [[nodiscard]] std::optional<LoadError> loadFile(const std::filesystem::path& lua_file, bool auto_run);
 
     static void pcall();
 
     template <typename... Args>
-    void push(Args&&... args)
-    {
-        if constexpr (sizeof...(Args) > 0) {
-            state_.push(std::forward<Args>(args)...);
-        }
-    }
+    void push(Args&&... args);
 
-    /**
-     * Can be used to define global variables or functions for usage in Lua script.
-     */
-    template <typename T>
-    void setGlobal(const std::string& variable_name, T&& value)
-    {
-        push(std::forward<T>(value));
-        state_.markGlobal(variable_name);
-    }
-
-    static std::string errorToString(int error_code);
+    [[nodiscard]] static std::string errorToString(int error_code);
 
     struct StackContext {
         // TODO: push error handling function to stack
@@ -98,6 +61,44 @@ private:
 private:
     LuaState state_;
 };
+
+template <typename ReturnValue, typename... Args>
+CallResult<ReturnValue> LuaScript::call(const std::string& function_name, Args&&... args)
+{
+    if (state_.pushGlobal(function_name)) {
+        if (auto function = state_.top<ReturnValue(Args...)>()) {
+            return (*function)(std::forward<Args>(args)...);
+        }
+        return CallError { CallError::Code::unknown, "Symbol does not match given type" };
+    }
+    return { CallError::Code::symbolNotFound };
+}
+
+template <typename VariableType>
+CallResult<VariableType> LuaScript::global(const std::string& variable_name)
+{
+    if (state_.pushGlobal(variable_name)) {
+        if (auto value = state_.pop<VariableType>()) {
+            return value.value();
+        }
+        return { CallError::Code::incorrectType };
+    }
+    return { CallError::Code::symbolNotFound };
+}
+template <typename VariableType>
+void LuaScript::global(const std::string& variable_name, VariableType&& new_value)
+{
+    push(std::forward<VariableType>(new_value));
+    state_.markGlobal(variable_name);
+}
+
+template <typename... Args>
+void LuaScript::push(Args&&... args)
+{
+    if constexpr (sizeof...(Args) > 0) {
+        state_.push(std::forward<Args>(args)...);
+    }
+}
 
 } // namespace ppplugin
 
--- include/ppplugin/noop_plugin.h
+++ include/ppplugin/noop_plugin.h
@@ -14,30 +14,33 @@ public:
     operator bool() const { return true; }
 
     template <typename ReturnValue, typename... Args>
-    CallResult<ReturnValue> call(std::string_view /*function_name*/, Args&&... /*args*/)
-    {
-        if constexpr (!std::is_void_v<ReturnValue>) {
-            if constexpr (std::is_default_constructible_v<ReturnValue>) {
-                return ReturnValue {};
-            } else {
-                static_assert(!sizeof(ReturnValue),
-                    "Return value of noop function must be default constructible!");
-            }
-        }
-        return CallResult<ReturnValue> {};
-    }
+    [[nodiscard]] CallResult<ReturnValue> call(std::string_view /*function_name*/, Args&&... /*args*/);
 
     template <typename VariableType>
-    CallResult<VariableType> global(const std::string& /*variable_name*/)
+    [[nodiscard]] CallResult<VariableType> global(const std::string& /*variable_name*/)
     {
         return VariableType {};
     }
     template <typename VariableType>
-    CallResult<void> global(const std::string& /*variable_name*/, VariableType&& /*new_value*/)
+    [[nodiscard]] CallResult<void> global(const std::string& /*variable_name*/, VariableType&& /*new_value*/)
     {
         return {};
     }
 };
+
+template <typename ReturnValue, typename... Args>
+CallResult<ReturnValue> NoopPlugin::call(std::string_view /*function_name*/, Args&&... /*args*/)
+{
+    if constexpr (!std::is_void_v<ReturnValue>) {
+        if constexpr (std::is_default_constructible_v<ReturnValue>) {
+            return ReturnValue {};
+        } else {
+            static_assert(!sizeof(ReturnValue),
+                "Return value of noop function must be default constructible!");
+        }
+    }
+    return CallResult<ReturnValue> {};
+}
 } // namespace ppplugin
 
 #endif // PPPLUGIN_NOOP_PLUGIN_H
